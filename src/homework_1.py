59393f9aa0e80ac59547bd363df47c153d17a2aa
Adam Arsanukaev <gtoressan@gmail.com>
2020-01-27 16:14:38 +0300

    Complete NumPy-part

--- src/homework_1.py
+++ src/homework_1.py
@@ -99,6 +99,111 @@ def task_5():
         print(f'The maximum is {a}\n')
 
 
+# Numpy/task 1.
+# Create two random arrays 'a' and 'b' with the same length.
+# Calculate the following distances between the arrays:
+# 1. Euclidean Distance.
+# 2. Manhattan Distance.
+# 3. Cosine Distance.
+def task_6():
+    print('Enter the length of arrays:', end=' ')
+    length = int(input())
+
+    a = np.random.rand(length)
+    b = np.random.rand(length)
+    euclidean_distance = np.linalg.norm(a - b)
+    manhattan_distance = (a - b).sum()
+    cosine_distance = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+    print(f'Euclidean Distance is {euclidean_distance}')
+    print(f'Manhattan Distance is {manhattan_distance}')
+    print(f'Cosine Distance is {cosine_distance}\n')
+
+
+# Numpy/task 2.
+# Create a random array with length of 10 and with sum of its elements equals to 2.
+def task_7():
+    a = np.random.rand(10)
+    a /= a.sum() / 2
+    print(f'The sum of array elements is {a.sum()}')
+    print(a)
+    switcher = input()
+
+
+# Numpy/task 3.
+# Create a random array with length of 100. Transform the array so, that:
+# 1. Maximum element(s) value is 1.
+# 2. Minimum element(s) value is 0
+# 3. Other values are in interval 0-1 with keeping the order.
+def task_8():
+    a = list(np.random.rand(100))
+    a[a.index(max(a))] = 1
+    a[a.index(min(a))] = 0
+    print(f'Maximum value is {max(a)}')
+    print(f'Minimum value is {min(a)}')
+    print(*a)
+    switcher = input()
+
+
+# Numpy/task 4.
+# Create a random array with length of 20 with integers from [0,50]. Select elements that are smaller than 5th
+# percentile and larger than 95th percentile
+def task_9():
+    a = np.random.randint(0, 50, size=20)
+    per5 = np.percentile(a, 5)
+    per95 = np.percentile(a, 95)
+    print(f'Elements smaller than 5th percentile {per5}:', a[a < per5])
+    print(f'Elements greeter than 95th percentile {per95}:', a[a > per95])
+    print(*a)
+    switcher = input()
+
+
+# Numpy/task 5.
+# Create an array with shape of {5; 6} with integer from 0 to 50. Print a column that contains the maximum
+# element of the array.
+def task_10():
+    a = np.random.randint(0, 50, size=[5, 6])
+    print(a)
+    print('Your column is:', a[:, a.argmax() % 6])
+    switcher = input()
+
+
+# Numpy/task 6.
+# Replace all missing values in the following array with mean
+def task_11():
+    arr = np.random.rand(10)
+    idx = np.random.randint(0, 10, 4)
+    arr[idx] = np.nan
+
+    print('Beginning array:', *arr)
+    arr_mean = arr[~np.isnan(arr)].mean()
+    arr[np.isnan(arr)] = arr_mean
+    print('Mean is', arr_mean)
+    print('New array:', *arr)
+    print()
+    switcher = input()
+
+
+# Numpy/task 7.
+# Download file 1 and file 2 into a directory with this notebook. Using function loadtxt in numpy load data from the
+# first file. Assign y = D[:,0] аnd X = D[:, 1:].
+# We will use one magic formula to find linear regression coefficients. You will prove this formula on your next
+# lectures of the course.
+def task_12():
+    D = np.loadtxt('../templates/tutorial_dataset_2.csv', skiprows=1, delimiter=',')
+    y = D[:, 0]
+    X = D[:, 1:]
+    b = np.linalg.inv(X.T @ X) @ X.T @ y
+    new_y = X @ b
+    res = y - new_y
+
+    plt.figure(figsize=(9, 6))
+    plt.scatter(new_y, res)
+    plt.show()
+
+    switcher = input()
+
+
 if __name__ == '__main__':
     while True:
-        task_5()
+        task_12()
